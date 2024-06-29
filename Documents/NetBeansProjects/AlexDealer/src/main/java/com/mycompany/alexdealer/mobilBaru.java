d384337678b8eb38cbf04661fbfaf63c38edc905
alexandrian15 <alexandrian6186@gmail.com>
2024-06-29 13:39:57 +0700

    Dealer Done

---
+++ Documents/NetBeansProjects/AlexDealer/src/main/java/com/mycompany/alexdealer/mobilBaru.java
@@ -0,0 +1,30 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.alexdealer;
+
+/**
+ *
+ * @author D-4
+ */
+class MobilBaru extends Mobil implements Kendaraan {
+    private final String garansi;
+    
+    public MobilBaru(String merek, String model, String garansi){
+        super(merek, model);
+        this.garansi = garansi;
+    }
+    
+    public String getGaransi(){
+        return garansi;
+    }
+    
+    @Override
+    public void info(){
+         System.out.println("Mobil Baru: " + getMerek() + " " + getModel() + "(Garansi: " + garansi + ")");
+    }
+}
+
